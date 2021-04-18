ba408c340df583db67c8470b8a7d2fcde6bb7346
Jederson Sousa Luz <jedersonalpha@gmail.com>
2021-04-18 17:57:16 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+
 import './App.css';
 
+import logo from './logo_wbg.svg';
+import api from './api.js';
+
 function App() {
+  const [file, setFile] = useState();
+
+  function handleSelectFile(event) {
+    if (!event.target.files) {
+      return;
+    }
+    setFile(event.target.files[0]);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData();
+
+    console.log(file);
+    data.append('file', file);
+
+    const status_code = await api.post('/uploadfile', data);
+
+    alert(`Arquivo enviado com sucesso! Status Code ${status_code.status}`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <label>ResumeAI</label>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input onChange={handleSelectFile} type="file" />
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
