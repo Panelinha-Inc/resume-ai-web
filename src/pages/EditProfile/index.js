d774642ff1cfe229a913d8a6298d660ed53a9017
Hélio Rocha Vieira de Couto Junior <junior0helio15@gmail.com>
2021-07-13 23:41:23 -0300

    Update index.js
    só pa ficar bunitin

--- src/pages/EditProfile/index.js
+++ src/pages/EditProfile/index.js
@@ -25,13 +25,13 @@ export default function EditProfile() {
 
     async function handleSubmit(event) {
         event.preventDefault();
-        
+
         if ((filePic !== user['profilePic']) || (name !== user['displayName'])) {
             const data = new FormData();
-            
+
             if (filePic === user['profilePic']) {
                 console.log('atim')
-                const blob = new Blob([user['profilePic']], {type: 'image/png'});
+                const blob = new Blob([user['profilePic']], { type: 'image/png' });
                 data.append('file', new File([blob], "omermo.png", {
                     type: blob.type,
                 }));
@@ -49,7 +49,7 @@ export default function EditProfile() {
                 }
             });
             if (status_code.status === 200) {
-                swal({title: `Alterações realizada com sucesso!`, text:`Status Code ${status_code.status}`, icon:'success'});
+                swal({ title: `Alterações realizadas com sucesso!`, text: `Status Code ${status_code.status}`, icon: 'success' });
                 user['displayName'] = name;
                 console.log(user)
                 ls.set('user-info', user)
@@ -61,9 +61,9 @@ export default function EditProfile() {
                 })
             }
         } else {
-            swal({title: `Sem alterações!`, icon: "info"});
+            swal({ title: `Sem alterações!`, icon: "info" });
         }
-    } 
+    }
 
     async function handleNewPass() {
         const status_code = await api.post('/resetpassword', {
@@ -85,49 +85,66 @@ export default function EditProfile() {
         }
     }
 
-    return (
-        <div className='App'>
-            <NavBar/>
-            <div className='App-body' style={{height : '88vh'}}>
-                <div className='card' style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 10 }}>
-                    <div className='column_profile'>
-                        <img
-                            src={urlImage}
-                            alt="User profile"
-                            onClick={() => fileInput.current.click()}
-                            style={{borderRadius: '100px', cursor: 'pointer', width: 100, height: 100}}
-                        />
-                        <input
-                            type='file'
-                            name='image'
-                            ref={fileInput}
-                            onChange={onChange}
-                            style={{ display: 'none' }}
-                        />
-                        <p>{user['email']}</p>
-                    </div>
-                    <div className='column_editable'>
-                        <form onSubmit={handleSubmit}>
-                            <span>
-                                
-                            <label for='fname' style={{float: 'left'}}>Nome:</label>
-                            <input
-                                name="name"
-                                type="name"
-                                value={name}
-                                onChange={e => setName(e.target.value)}
-                                placeholder="Nome"
-                                id='fname'
-                            />
-                            </span>
-                            <br></br>
-                            <button className='button_forma'>Salvar alterações</button>
-                        </form>
-                        <p>Esqueceu a senha?</p>
-                        <button onClick={handleNewPass} className='button_forma'>Redefinir senha</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+    return ( <
+        div className = 'App' >
+        <
+        NavBar / >
+        <
+        div className = 'App-body'
+        style = {
+            { height: '88vh' } } >
+        <
+        div className = 'card'
+        style = {
+            { display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 10 } } >
+        <
+        div className = 'column_profile' >
+        <
+        img src = { urlImage }
+        alt = "User profile"
+        onClick = {
+            () => fileInput.current.click() }
+        style = {
+            { borderRadius: '100px', cursor: 'pointer', width: 100, height: 100 } }
+        /> <
+        input type = 'file'
+        name = 'image'
+        ref = { fileInput }
+        onChange = { onChange }
+        style = {
+            { display: 'none' } }
+        /> <
+        p > { user['email'] } < /p> <
+        /div> <
+        div className = 'column_editable' >
+        <
+        form onSubmit = { handleSubmit } >
+        <
+        span >
+
+        <
+        label
+        for = 'fname'
+        style = {
+            { float: 'left' } } > Nome: < /label> <
+        input name = "name"
+        type = "name"
+        value = { name }
+        onChange = { e => setName(e.target.value) }
+        placeholder = "Nome"
+        id = 'fname' /
+        >
+        <
+        /span> <
+        br > < /br> <
+        button className = 'button_forma' > Salvar alterações < /button> <
+        /form> <
+        p > Esqueceu a senha ? < /p> <
+        button onClick = { handleNewPass }
+        className = 'button_forma' > Redefinir senha < /button> <
+        /div> <
+        /div> <
+        /div> <
+        /div>
     )
 }
